a865a7e9768b70e8f3d8a800df0d79b385cb3d70
quentg77 <q.guichaoua@gmail.com>
2020-03-02 15:00:36 +0100

    [ARCHI](config) ts + babel

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = api => {
+	api.cache(true)
+
+	const presets = ['@babel/preset-env', '@babel/preset-typescript']
+	const plugins = [
+		"@babel/plugin-transform-runtime",
+		["@babel/plugin-proposal-decorators", { "legacy": true }],
+		["@babel/plugin-proposal-class-properties", { "loose": true }]
+	]
+
+	return {
+		presets,
+		plugins
+	}
+}
